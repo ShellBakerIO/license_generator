839da5bc4f8693754e1e40d492001f43f9a5c1e3
K0ch3rga <mr.skel@mail.ru>
2024-08-06 17:32:33 +0500

    fix: requirements

--- auth/main.py
+++ auth/main.py
@@ -7,9 +7,9 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from loguru import logger
 from sqlalchemy.orm import Session, sessionmaker
 
-from auth.crud import create_user
-from auth.ldap import authenticate
-from auth.models import engine, Base, User
+from crud import create_user
+from ldap import authenticate
+from models import engine, Base, User
 
 app = FastAPI(title="AuthService")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
