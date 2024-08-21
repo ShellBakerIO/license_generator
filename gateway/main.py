feb4084c0c5789e974baf9bd04ec33f95dfaff65
ShellBakerIO <shellbaker123@gmail.com>
2024-08-21 15:41:56 +0500

    added: Content-Disposition in CORS, checking validity of the token

--- gateway/main.py
+++ gateway/main.py
@@ -17,7 +17,8 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*", "Content-Disposition"],
+    expose_headers={"Content-Disposition"},
 )
 
 
