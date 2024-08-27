ad97b939751595f033de796b59659317af4f77aa
ShellBakerIO <shellbaker123@gmail.com>
2024-08-27 10:00:55 +0500

    add: decode and hash password

--- gateway/api_wrapper.py
+++ gateway/api_wrapper.py
@@ -1,14 +1,12 @@
 import os
 from functools import wraps
 from typing import Any, Optional, Union
-
 import aiohttp
 import jwt
 from aiohttp.formdata import FormData
 from fastapi import Request, Response, HTTPException, status
 from fastapi.responses import FileResponse
 from fastapi.routing import APIRouter
-
 import crud
 
 router = APIRouter()
@@ -26,15 +24,14 @@ def gateway_router(method,
         @app_method
         @wraps(endpoint)
         async def decorator(request: Request, response: Response, **kwargs):
-            scope = request.scope
-            headers = {}
-            request_method = scope['method'].lower()
-            path = scope['path']
-            payload = kwargs.get(payload_key)
-            data = crud.form_data(kwargs, payload, payload_key)
-            url = crud.form_url(service_url, path, kwargs)
-
             if access_level is None:
+                scope = request.scope
+                headers = {}
+                request_method = scope['method'].lower()
+                path = scope['path']
+                payload = kwargs.get(payload_key)
+                data = crud.form_data(kwargs, payload, payload_key)
+                url = crud.form_url(service_url, path, kwargs)
                 response_data = await send_request(
                     url=url,
                     method=request_method,
@@ -44,15 +41,20 @@ def gateway_router(method,
                 return response_data
             else:
                 try:
-                    decoded_token = jwt.decode(
-                        kwargs.get('token'),
-                        os.getenv("SECRET_KEY"),
-                        algorithms=['HS256'],
-                        options={'verify_aud': True}
-                    )
+                    decoded_token = jwt.decode(kwargs.get('token'),
+                                               os.getenv("SECRET_KEY"),
+                                               algorithms=['HS256'],
+                                               options={'verify_aud': True}
+                                               )
                     has_access = decoded_token["claims"]
-
                     if access_level in has_access:
+                        scope = request.scope
+                        headers = {}
+                        request_method = scope['method'].lower()
+                        path = scope['path']
+                        payload = kwargs.get(payload_key)
+                        data = crud.form_data(kwargs, payload, payload_key)
+                        url = crud.form_url(service_url, path, kwargs)
                         response_data = await send_request(
                             url=url,
                             method=request_method,
@@ -62,7 +64,7 @@ def gateway_router(method,
                         return response_data
                     else:
                         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No access")
-                except jwt.InvalidSignatureError or jwt.DecodeError:
+                except jwt.InvalidSignatureError or jwt.exceptions.DecodeError:
                     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
     return wrapper
@@ -71,7 +73,6 @@ def gateway_router(method,
 async def send_request(url: str, method: str, data: Union[dict, FormData], headers: dict = None):
     if headers is None:
         headers = {}
-
     async with aiohttp.ClientSession() as session:
         request = getattr(session, method)
         if isinstance(data, dict):
@@ -82,11 +83,9 @@ async def send_request(url: str, method: str, data: Union[dict, FormData], heade
                 elif 'text/plain' in content_type or 'application/octet-stream' in content_type:
                     file_name = response.headers.get('Content-Disposition').split("filename=")[1].strip('"')
                     file_content = await response.read()
-
                     temp_file_path = f'/tmp/{file_name}'
                     with open(temp_file_path, 'wb') as f:
                         f.write(file_content)
-
                     return FileResponse(path=temp_file_path, filename=file_name)
                 else:
                     data = await response.read()
@@ -98,13 +97,10 @@ async def send_request(url: str, method: str, data: Union[dict, FormData], heade
                 elif 'text/plain' in content_type or 'application/octet-stream' in content_type:
                     file_name = response.headers.get('Content-Disposition').split("filename=")[1].strip('"')
                     file_content = await response.read()
-
                     temp_file_path = f'/tmp/{file_name}'
                     with open(temp_file_path, 'wb') as f:
                         f.write(file_content)
-
                     return FileResponse(path=temp_file_path, filename=file_name)
                 else:
                     data = await response.read()
-
         return data
