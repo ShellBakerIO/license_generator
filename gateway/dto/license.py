338e7082845c405bc136abf964288447b3f2be54
K0ch3rga <mr.skel@mail.ru>
2024-08-27 14:18:42 +0500

    fix: final

--- gateway/dto/license.py
+++ gateway/dto/license.py
@@ -9,6 +9,7 @@ class LicensesInfo(BaseModel):
     product_name: str
     license_users_count: int
     exp_time: str
+    additional_license_information: str
 
     @classmethod
     def as_form(
@@ -17,9 +18,13 @@ class LicensesInfo(BaseModel):
         product_name: str = Form(...),
         license_users_count: int = Form(...),
         exp_time: str = Form(str(date(day=20, month=4, year=2152))),
+        additional_license_information: str = Form(...),
     ):
 
-        return cls(company_name=company_name,
-                   product_name=product_name,
-                   license_users_count=license_users_count,
-                   exp_time=exp_time)
+        return cls(
+            company_name=company_name,
+            product_name=product_name,
+            license_users_count=license_users_count,
+            exp_time=exp_time,
+            additional_license_information=additional_license_information,
+        )
